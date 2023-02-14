b89d933eb94e1081b4bfe096ccc8413239f5dc8f
vectorzhao <1092381273@qq.com>
2023-02-14 23:05:54 +0800

    Changes

--- deepexo/main.py
+++ deepexo/main.py
@@ -3,6 +3,18 @@ import numpy as np
 
 import mdn
 import joblib
-
+from numpy.typing import ArrayLike
 class RockyPlanet:
-    def predict(self, ):
+    def predict(self, planet_params: ArrayLike):
+        """
+        Args:
+            Array of floats of the planet parameters
+            in the following order: [M, R, k2].
+            The following input ranges are supported:
+                M: 0.1 < M [M_earth] < 30
+                R: 0.1 < R [R_earth] < 30
+                k2: 0 < k2 < 0.5
+        Returns:
+
+        """
+
