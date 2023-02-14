6125bdb467acc231e336e51f29592a3159f9389a
vectorzhao <1092381273@qq.com>
2023-02-14 22:50:01 +0800

    Changes

---
+++ deepexo/__init__.py
@@ -0,0 +1,1 @@
+from deepexo.main import PlanetSynth
---
+++ deepexo/main.py
@@ -0,0 +1,8 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+
+import mdn
+import joblib
+
+class RockyPlanet:
+    def predict(self, ):
